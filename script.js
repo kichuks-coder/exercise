d3dc379c193f81bf672bb0354d884f98acacaf45
Sergey <kichuks@mail.ru>
2023-04-05 11:48:14 +0300

    exersice

---
+++ script.js
@@ -0,0 +1,14 @@
+let money = +prompt("Ваш бюджет на месяц?");
+let time = +prompt("Введите дату в формате YYYY-MM-DD");
+let timeData = time;
+let answer1, answer2;
+answer1 = +prompt("Введите лбязательную статью расходов на этот месяц");
+answer1 = +prompt("Введите лбязательную статью расходов на этот месяц");
+answer2 = +prompt("Во сколько обойдется?");
+answer2 = +prompt("Во сколько обойдется?");
+let expenses = {"answer1":"answer2"};
+// console.log(appData);
+let optionalExpenses = {};
+let income = [];
+let savings = false;
+let appData = {money, timeData, expenses, optionalExpenses, income};
